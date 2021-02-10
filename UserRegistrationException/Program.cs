2602d360fe0997dccd519b850bdcc3d1a57e5afe
diliprathod32 <diliprathod32@gmail.com>
2021-02-10 22:21:42 +0530

    [Dilip] WelcomeToCustomeDefinedExceptionOnUserRegistration

---
+++ UserRegistrationException/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UserRegistrationException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User defined Exception on user registration!!! ");
+        }
+    }
+}
